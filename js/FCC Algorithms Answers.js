9c50521fe3575109a35052556603d8d56e7aee85
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-12-05 00:36:06 +0700

    Add Selection Sort and Insertion Sort (Algorithms)

--- js/FCC Algorithms Answers.js
+++ js/FCC Algorithms Answers.js
@@ -15,3 +15,35 @@ function bubbleSort(array) {
 
   return array;
 }
+
+// Implement Selection Sort
+function selectionSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    let minIdx = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIdx]) minIdx = j;
+    }
+
+    if (minIdx !== i) [array[i], array[minIdx]] = [array[minIdx], array[i]];
+  }
+
+  return array;
+}
+
+// Implement Insertion Sort
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    const takenVal = array[i];
+    let holeIdx = i;
+
+    while (holeIdx > 0 && array[holeIdx - 1] > takenVal) {
+      array[holeIdx] = array[holeIdx - 1];
+      holeIdx--;
+    }
+
+    array[holeIdx] = takenVal;
+  }
+
+  return array;
+}
